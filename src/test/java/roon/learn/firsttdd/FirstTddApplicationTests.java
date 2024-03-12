d27985d17e65205f9e044ca39f073a860051ee28
roon-replica <think-roon@naver.com>
2024-03-12 23:02:55 +0900

    initial commit

---
+++ src/test/java/roon/learn/firsttdd/FirstTddApplicationTests.java
@@ -0,0 +1,13 @@
+package roon.learn.firsttdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
