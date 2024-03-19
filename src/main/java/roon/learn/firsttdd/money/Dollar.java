7a1896b101ca4e2c47db7f65f263b3a98ef667be
roon-replica <think-roon@naver.com>
2024-03-19 13:40:46 +0900

    [money 예제 ch.1] 곱셈 테스트. 테스트 통과하게 만듦

---
+++ src/main/java/roon/learn/firsttdd/money/Dollar.java
@@ -0,0 +1,14 @@
+package roon.learn.firsttdd.money;
+
+public class Dollar {
+
+	int amount;
+
+	public Dollar(int amount) {
+		this.amount = amount;
+	}
+
+	public void times(int multiplier) {
+		this.amount *= multiplier;
+	}
+}
---
+++ src/test/java/roon/learn/firsttdd/money/MoneyTest.java
@@ -0,0 +1,16 @@
+package roon.learn.firsttdd.money;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+	@Test
+	void testMultiplication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
+
+}
